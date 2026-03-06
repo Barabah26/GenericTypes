5cefb4ba6ebf5ce1811e1f96272f4fc6594a9f83
Pavlo Barabakh <pavlo.barabakh@everymatrix.com>
2026-03-06 15:52:20 +0200

    Coding Exercise 31: Generic types - Pair class

---
+++ GenericTypes/Exercise/Pair.cs
@@ -0,0 +1,15 @@
+﻿namespace GenericTypes.Exercise;
+public class Pair<T>
+{
+    public T First { get; private set; }
+    public T Second { get; private set; }
+
+    public Pair(T first, T second)
+    {
+        First = first;
+        Second = second;
+    }
+
+    public T ResetFirst() => First = default;
+    public T ResetSecond() => Second = default;
+}
