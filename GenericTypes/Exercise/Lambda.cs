297ca07833e2e71d54b75b281b4d88cf0742a1c9
Pavlo Barabakh <pavlo.barabakh@everymatrix.com>
2026-03-09 22:20:51 +0200

    Coding Exercise 35: Basics of lambda expressions

---
+++ GenericTypes/Exercise/Lambda.cs
@@ -0,0 +1,7 @@
+﻿namespace GenericTypes.Exercise;
+
+public class Lambda
+{
+    public Func<string, int> GetLength = s => s.Length;
+    public Func<int> GetRandomNumberBetween1And10 = () => new Random().Next(1, 10);
+}
